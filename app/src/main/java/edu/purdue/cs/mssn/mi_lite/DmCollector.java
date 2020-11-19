f8bc5af0ba2f85ce0760cfb776f65734ffddc7d3
Haotian Deng <moonsky219@live.cn>
2020-11-19 11:51:27 -0500

    Delete DmCollector.java in app module

--- app/src/main/java/edu/purdue/cs/mssn/mi_lite/DmCollector.java
+++
@@ -1,9 +0,0 @@
-package edu.purdue.cs.mssn.mi_lite;
-
-public class DmCollector {
-    static {
-        System.loadLibrary("dmcollector");
-    }
-
-    public static native byte[] generateCustomPkt(String message);
-}
